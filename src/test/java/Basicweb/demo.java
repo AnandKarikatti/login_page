571299ffb3c1c11edfa485f31b58344e4ab5dbb4
AnandKarikatti <anand.kk341@gmail.com>
2023-12-07 11:34:28 +0530

    Updatefirst

--- src/test/java/Basicweb/demo.java
+++ src/test/java/Basicweb/demo.java
@@ -1,12 +1,12 @@
 package Basicweb;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
-public class Firsttestclass {
-
-	public static void main(String[] args) throws InterruptedException {
+public class demo {
+	
+public static void main(String[] args) throws InterruptedException {
 		
 		WebDriver driver = new ChromeDriver();
 
